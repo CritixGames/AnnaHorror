1f3981f877c17653fd42acde7090a12deaff7cdb
Cristian Gasparesc (CritixGames) <93558364+CritixGames@users.noreply.github.com>
2022-09-17 15:29:43 -0400

    Start

---
+++ Assets/Scripts/Doors.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour
+{
+    public Animator door;
+    public GameObject openText;
+
+    public AudioSource doorSound;
+
+
+    public bool inReach;
+
+
+
+
+    void Start()
+    {
+        inReach = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            openText.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            openText.SetActive(false);
+        }
+    }
+
+
+
+
+
+    void Update()
+    {
+
+        if (inReach && Input.GetButtonDown("Interact"))
+        {
+            DoorOpens();
+        }
+
+        else if (inReach && Input.GetButtonDown("Interact"))
+        {
+            DoorCloses();
+        }
+
+
+
+
+    }
+    void DoorOpens ()
+    {
+        Debug.Log("It Opens");
+        door.SetBool("Open", true);
+        door.SetBool("Closed", false);
+        doorSound.Play();
+
+    }
+
+    void DoorCloses()
+    {
+        Debug.Log("It Closes");
+        door.SetBool("Open", false);
+        door.SetBool("Closed", true);
+        doorSound.Play();
+    }
+
+
+}
